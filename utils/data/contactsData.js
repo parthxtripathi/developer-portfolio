bc59e5dde4f5b9465f61de768218e36f95fcf731
Parth Tripathi <parthdon03@gmail.com>
2024-10-16 19:04:10 +0530

    contacts data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,9 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'parthxtripathi@gmail.com',
+    phone: '+919990823607',
+    address: 'New Delhi, India ',
+    github: 'https://github.com/parthxtripathi',
+    linkedIn: 'https://www.linkedin.com/in/parth-tripathi-689506202/',
+    twitter: 'https://x.com/parthxtripathi',
+    stackOverflow: 'https://stackoverflow.com/users/22012579/parth-tripathi',
 }
